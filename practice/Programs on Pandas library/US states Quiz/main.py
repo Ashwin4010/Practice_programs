577bce9d39d3635304560949081521aaac3999bb
ashwin4010 <ashwinpremnath123@gmail.com>
2022-04-11 20:27:47 +0530

    list comprehension in python with samples

--- practice/Programs on Pandas library/US states Quiz/main.py
+++ practice/Programs on Pandas library/US states Quiz/main.py
@@ -16,13 +16,11 @@ while len(guessed_states)<50:
     print(answer_state)
     if answer_state == "Exit":
         missing_states = []
-        for state in all_state:
-            if state not in guessed_states:
-                missing_states.append(state)
-        print(missing_states)
-        new_data = pd.DataFrame(missing_states)
-        new_data.to_csv("States_to_learn.csv", index=None)
+        new_data = [missing_states.append(state) for state in all_state if state not in guessed_states]
+        data = pd.DataFrame(missing_states)
+        data.to_csv("states_to_learn.csv", index_label="states")
         break
+
     if answer_state in all_state:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
