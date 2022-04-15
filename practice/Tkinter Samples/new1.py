8e1cf7dd1da48beedb77535051d2a1e67879aee3
ashwin4010 <ashwinpremnath123@gmail.com>
2022-04-16 02:03:21 +0530

    Tkinter Sample Programs

--- practice/Tkinter Samples/new1.py
+++ practice/Tkinter Samples/new1.py
@@ -1,13 +1,32 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("First GUI")
 window.minsize(width=500,height=500)
 
 #label
 
-label = tkinter.Label(text="I am a Label",font=("Arial",24))
-label.pack(expand=True)
+label = Label(text="I am a Label",font=("Arial",24))
+label.pack()
+
+label["text"] = "new Text"
+label.config(text="new text")
+
+def button_clicked():
+    print("I got Clicked")
+    new_text = input.get()
+    label.config(text=new_text)
+
+
+button = Button(text="Click Me",command=button_clicked)
+button.pack()
+
+
+#Entry component
+
+input = Entry(width=40)
+input.pack()
+
 
 
 window.mainloop()
