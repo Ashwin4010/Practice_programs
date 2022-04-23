f030c127cbdad3af7b09eb0715cb41c0cd14b95d
ashwin4010 <ashwinpremnath123@gmail.com>
2022-04-24 01:49:40 +0530

    Done: 1. Send a Motivational Quote on Monday

--- practice/Email via python/main.py
+++ practice/Email via python/main.py
@@ -1,10 +1,18 @@
 import smtplib
+import datetime as dt
+import random
 
-my_mail = "ashwinpremnath4010@gmail.com"
-password = "amuthachandran"
+now = dt.datetime.now()
+with open("quotes.txt","r") as quotes:
+    lines = quotes.readlines()
+    quote_to_send = random.choice(lines)
+    if dt.date.today().weekday() == 0:
 
-connection = smtplib.SMTP("smtp.gmail.com",587)
-connection.starttls()
-connection.login(user=my_mail,password=password)
-connection.sendmail(from_addr=my_mail, to_addrs="ashwinpremnath123@gmail.com",msg="Hello")
-connection.close()
+        my_mail = "ashwinpremnath4010@gmail.com"
+        password = "amuthachandran"
+
+        connection = smtplib.SMTP("smtp.gmail.com",587)
+        connection.starttls()
+        connection.login(user=my_mail,password=password)
+        connection.sendmail(from_addr=my_mail, to_addrs="ashwinpremnath123@gmail.com",msg=f"{quote_to_send}")
+        connection.close()
