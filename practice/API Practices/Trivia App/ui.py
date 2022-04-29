5fd909c1bf6939579a4c363e9bd9489adf505584
ashwin4010 <ashwinpremnath123@gmail.com>
2022-04-29 23:59:26 +0530

    Quiz App (Fixed Some Minor Bugs)

--- practice/API Practices/Trivia App/ui.py
+++ practice/API Practices/Trivia App/ui.py
@@ -32,21 +32,23 @@ class QuizInterface:
         self.true_button = Button(image=true_image, highlightthickness=0,command=self.true_pressed)
         self.true_button.grid(row=2, column=0)
 
-        self.true_button = Button(image=false_image, highlightthickness=0,command=self.false_pressed)
-        self.true_button.grid(row=2, column=1)
+        self.false_button = Button(image=false_image, highlightthickness=0,command=self.false_pressed)
+        self.false_button.grid(row=2, column=1)
 
         self.get_next_question()
 
         self.window.mainloop()
 
     def get_next_question(self):
+        self.canvas.config(bg="white")
         if self.quiz.still_has_questions():
-            self.canvas.config(bg="white")
             self.score_label.config(text=f"Score: {self.quiz.score}")
             q_text = self.quiz.next_question()
             self.canvas.itemconfig(self.question_text, text=q_text)
         else:
             self.canvas.itemconfig(self.question_text,text="You've reached the end of the quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
     def true_pressed(self):
         self.give_feedback(self.quiz.check_answer("True"))
